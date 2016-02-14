d59d713eb28204a626203a6f7eee9abd06e6ba0a
Josh Hollandsworth <jshollandsworth@gmail.com>
2016-02-13 22:57:57 -0500

    changed to admin theme due to amount of information needed for display

--- src/app/urls.py
+++ src/app/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.index, name='index'),
+    url(r'^characters/$', views.CharacterList.as_view(), name='character_list'),
+    url(r'^characters/(?P<pk>[0-9]+)/$', views.CharacterDetail.as_view(), name='character_detail'),
 ]
--- src/app/views.py
+++ src/app/views.py
@@ -1,5 +1,13 @@
 from django.shortcuts import render
+from app.models import *
+from django.views.generic import *
 
 
 def index(request):
     return render(request, 'app/index.html', {})
+
+class CharacterList(ListView):
+    model = Character
+
+class CharacterDetail(DetailView):
+    model = Character
